d031bc10d08597149b6fe0051fe53580381d1dc0
elenvs83 <elenvs83@gmail.com>
2026-04-16 15:18:22 +0300

    fix bugs and add sqrt

--- calc.py
+++ calc.py
@@ -1,4 +1,6 @@
-# Автор: ...
+# Автор: Сапунова Елена
+
+import math
 
 def add(a, b):
     return a + b
@@ -9,10 +11,15 @@ def subtract(a, b):
 
 
 def multiply(a, b):
-    # TODO: не реализовано
-    return None
+    return a * b
 
+
+def sqrt(x):
+    return math.sqrt(x)
 
 if __name__ == "__main__":
     print("Простой калькулятор запущен.")
     print(f"2 + 2 = {add(2, 2)}")
+    print(f"2 - 2 = {subtract(2, 2)}")
+    print(f"2 * 2 = {multiply(2, 2)}")
+    print(f"Корень из 4 = {sqrt(4)}")
