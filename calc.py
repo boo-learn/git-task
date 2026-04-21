2b3b266ce68472726f9fbdfbe8f9014c82c77bae
Максим Мочаков <mochakov.ma@ya.ru>
2026-04-21 09:08:48 +0300

    fix bugs and add sqrt, outputs

--- calc.py
+++ calc.py
@@ -1,18 +1,21 @@
-# Автор: ...
+# Автор: Максим Мочаков
 
+import math
 def add(a, b):
     return a + b
-
 
 def subtract(a, b):
     return a - b
 
-
 def multiply(a, b):
     # TODO: не реализовано
-    return None
+    return a * b
 
+def sqrt(x):
+    return math.sqrt(x)
 
 if __name__ == "__main__":
     print("Простой калькулятор запущен.")
     print(f"2 + 2 = {add(2, 2)}")
+    print(f"3 * 4 = {multiply(3, 4)}") 
+    print(f"sqrt(16) = {sqrt(16)}")
